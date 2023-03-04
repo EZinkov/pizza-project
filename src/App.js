4d77837c96cc33d06d6a42add54a0a70ac56f0fa
EZinkov <evgeniyzinkov98@gmail.com>
2023-03-04 18:37:30 +0100

    Add PizzaBlock component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import { useEffect, useState } from "react"
 import Categories from "./components/Categories"
 import Header from "./components/Header"
+import PizzaBlock from "./components/PizzaBlock"
 import Sort from "./components/Sort"
 import "./scss/app.scss"
+
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("https://64037b3d302b5d671c50a3e4.mockapi.io/items")
+      .then(res => res.json())
+      .then(data => setData(data))
+  }, [])
+
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
+          <h2 className="content__title">All pizzas</h2>
+
+          <div className="content__items">
+            {data.map(pizza => (
+              <PizzaBlock key={pizza.id} {...pizza} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
