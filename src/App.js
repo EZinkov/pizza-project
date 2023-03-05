f7ec51678858eebc50dc6937a758ab0467e7b5d8
EZinkov <evgeniyzinkov98@gmail.com>
2023-03-05 13:42:40 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-import { useEffect, useState } from "react"
-import Categories from "./components/Categories"
+import { Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
-import PizzaBlock from "./components/PizzaBlock"
-import Sort from "./components/Sort"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import Cart from "./pages/Cart"
 import "./scss/app.scss"
 
 function App() {
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    fetch("https://64037b3d302b5d671c50a3e4.mockapi.io/items")
-      .then(res => res.json())
-      .then(data => setData(data))
-  }, [])
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">All pizzas</h2>
-
-          <div className="content__items">
-            {data.map(pizza => (
-              <PizzaBlock key={pizza.id} {...pizza} />
-            ))}
-          </div>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   )
