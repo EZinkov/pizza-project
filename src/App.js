8b2342b1c880426d146632da6ff26670b093333a
EZinkov <evgeniyzinkov98@gmail.com>
2023-03-06 17:27:42 +0100

    add: context for input

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
+import { createContext, useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
 import { Route, Routes } from "react-router-dom"
+import { decrement, increment } from "./redux/slices/filterSlice"
+
 import Header from "./components/Header"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import Cart from "./pages/Cart"
 import "./scss/app.scss"
 
+export const SearchContext = createContext("")
+
 function App() {
+  const [searchValue, setSearchValue] = useState("")
+
   return (
     <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </SearchContext.Provider>
     </div>
   )
 }
