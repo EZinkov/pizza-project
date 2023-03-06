2852581f96efa85c69079d85b77f058f461c962b
EZinkov <evgeniyzinkov98@gmail.com>
2023-03-06 17:31:31 +0100

    add: redux logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,58 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
+import { setCategoryId } from "../redux/slices/filterSlice"
 import Categories from "../components/Categories"
-import PizzaBlock from "../components/PizzaBlock/PizzaBlock"
+import PizzaBlock from "../components/PizzaBlock"
 import Skeleton from "../components/PizzaBlock/Skeleton"
 import Sort from "../components/Sort"
+import Pagination from "../components/Pagination"
+import { SearchContext } from "../App"
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const { categoryId, sort } = useSelector(state => state.filter)
+
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
+  const { searchValue } = useContext(SearchContext)
+
+  const handleCategory = id => {
+    dispatch(setCategoryId(id))
+  }
+
+  const pizzas = data.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)
+  const skeletons = [...new Array(8)].map((_, index) => (
+    <Skeleton key={index} />
+  ))
+
   useEffect(() => {
-    fetch("https://64037b3d302b5d671c50a3e4.mockapi.io/items")
+    setIsLoading(true)
+
+    const search = searchValue ? `&search=${searchValue}` : ""
+    const category = categoryId > 0 ? `category=${categoryId}` : ""
+
+    fetch(
+      `https://64037b3d302b5d671c50a3e4.mockapi.io/items?page=1&${category}&sortBy=${sort.sortProperty}&order=desc${search}`
+    )
       .then(res => res.json())
       .then(data => {
         setData(data)
         setIsLoading(false)
       })
     window.scrollTo(0, 0)
-  }, [])
+  }, [categoryId, sort.sortProperty, searchValue])
+
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
+        <Categories value={categoryId} handleCategory={handleCategory} />
         <Sort />
       </div>
       <h2 className="content__title">All pizzas</h2>
-
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
-          : data.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination />
     </div>
   )
 }
