fa6c18c6782c2d7a5d0d792a13abf5829d3c317e
EZinkov <evgeniyzinkov98@gmail.com>
2023-03-07 19:23:27 +0100

    add: empty cart component

---
+++ src/components/CartEmpty.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import emptyCartImg from "../assets/img/empty-cart.png"
+
+const CartEmpty = () => {
+  return (
+    <>
+      <div class="cart cart--empty">
+        <h2>
+          Empty cart <icon>😕</icon>
+        </h2>
+        <p>
+          You probably haven't ordered pizza yet. <br />
+          To order pizza, go to the main page.{" "}
+        </p>
+        <img src={emptyCartImg} alt="Empty cart" />
+        <Link to="/" className="button button--black">
+          <span>Go back</span>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default CartEmpty
